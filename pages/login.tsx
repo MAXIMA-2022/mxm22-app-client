8325f94f819dcee15fdba827ae74c930de3f6e6f
palsarip <naufal.syarif0809@gmail.com>
2022-08-16 23:07:45 +0700

    feat: login & register page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-children-prop */
 import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,83 +8,151 @@ import React, { useState, useEffect, useRef } from "react";
 //importing local components
 import Layout from "../components/Layout";
 import Navbar from "../components/Navbar";
-import MaximaIconP from "../public/maximaIconP.svg";
-import MaximaIconP2 from "../public/maximaIconP2.svg";
+// import MaximaIcon from "../public/Register/MaximaLogo.svg";
 
 //importing chakra ui components
-import { Box, Flex, Center, Heading, Text, Button, Stack, Img, Tabs, TabList, TabPanels, Tab, TabPanel, Container, List, ListItem, ListIcon, OrderedList, UnorderedList } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Center,
+  Heading,
+  Text,
+  Button,
+  Stack,
+  Img,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Container,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Select,
+  InputLeftAddon,
+  InputRightAddon,
+} from "@chakra-ui/react";
 
 const login: NextPage = () => {
-  const LinkButton = () => {
-    const linkData = [
-      {
-        name: "HoME (Hall of Maxima Exhibition) ",
-        link: "aboutus/home",
-      },
-      {
-        name: "STATE (Student Activities Unit Explore)",
-        link: "aboutus/state",
-      },
-      {
-        name: "Malam Puncak",
-        link: "aboutus/malampuncak",
-      },
-    ];
-
+  const MaximaLogo = () => {
     return (
-      <Box mt={["1.5em", "1.5em", "2vh", "0.2em", "0.8em"]}>
-        <Container maxW="full">
-          {linkData.map((item, index) => {
-            return (
-              <Center key={index}>
-                <Link href={item.link}>
-                  <Button
-                    w={["full", "auto"]}
-                    my={["0.7em", "0.7em", "0.7em", "0.7em", "0.7em"]}
-                    p={["0 1.5em", "0 1.5em", "2vh 1.5em", "0 1.5em", "0 1.5em"]}
-                    size={["xs", "sm"]}
-                    bgColor={"#FF6835"}
-                    color={"white"}
-                    borderRadius={"full"}
-                    boxShadow={"0px 2px 2px 2px rgb(0,0,0,0.25)"}
-                    zIndex={"1"}
-                  >
-                    <Text fontSize={["xs", "sm", "2.2vh", "sm", "lg"]}>{item.name}</Text>
+      <Center mt={["-3vh", "5vh"]} position={["relative", "absolute"]} left={0} right={0} top={0}>
+        <Img display={["none", "block"]} src={"/Register/MaximaLogo.svg"} w={["9rem"]} />
+        <Img display={["block", "none"]} src={"/Register/MaximaLogo2.svg"} w={["3rem"]} />
+      </Center>
+    );
+  };
+
+  const LoginForm = () => {
+    return (
+      <Flex w={"full"} maxW={["65em", "65em", "65em", "55em", "65em"]} maxH={"auto"} mx={"3em"}>
+        <Flex w={"full"} h={"auto"} justifyContent={"space-between"}>
+          <Center me={"2em"}>
+            <Img src={"/Login/gambar-masuk.png"} w={["38em", "38em", "30em", "30em", "38em"]} />
+          </Center>
+          <Box
+            w={["full", "22em"]}
+            h={["full", "auto"]}
+            padding={["0 2em", "1.5em 2.5em 1em 2.5em"]}
+            borderRadius={["none", "lg"]}
+            boxShadow={["none", "-1.2px 5px 4px 0px rgb(0,0,0,0.25)"]}
+            bgColor={"#fff"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            blur={[0, 15]}
+            border={["none", "1px solid rgb(27, 65, 115, 0.25)"]}
+            overflowY={"auto"}
+            zIndex={1}
+          >
+            <Center>
+              <Text fontSize={["3xl", "3xl", "3xl", "2xl", "3xl"]} fontWeight={"bold"} color={"#1B4173"}>
+                Masuk
+              </Text>
+            </Center>
+            <Center mb={"1em"}>
+              <Text fontSize={["md", "md", "md", "sm", "md"]} color={"#1B4173"} fontWeight={"medium"}>
+                Belum punya akun? <span style={{ color: "#F7B70C", fontWeight: "bold", textDecoration: "underline" }}>Daftar</span>
+              </Text>
+            </Center>
+            <Box>
+              <form>
+                <FormControl>
+                  <Stack direction={["column"]} spacing={[5, 4]}>
+                    <Box w={"full"}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        NIM
+                      </FormLabel>
+                      <InputGroup>
+                        <InputLeftAddon fontSize={"sm"} m={"auto"} p={2} children={"000000"} bgColor={"#F7B70C"} color={"white"} borderRadius={"full"} />
+                        <Input
+                          size={"md"}
+                          borderLeft={"none"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={""}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box w={"full"}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Nama Lengkap
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"Nama Lengkap"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={["full", "full"]}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                      <Text fontSize={["sm"]} my={"0.5em"} color={"#1B4173"} fontWeight={"medium"}>
+                        Lupa kata sandimu? <span style={{ color: "#F7B70C", fontWeight: "bold", textDecoration: "underline" }}>Klik di sini</span>
+                      </Text>
+                    </Box>
+                  </Stack>
+                </FormControl>
+                <Flex w={"100%"} justifyContent={"center"} mt={"2em"} mb={"1em"}>
+                  <Button w={["full", "auto"]} px={["2.1em"]} borderRadius={"full"} type={"submit"} color={"#fff"} colorScheme={"orange"} bgColor={"#F7B70C"}>
+                    MASUK
                   </Button>
-                </Link>
-              </Center>
-            );
-          })}
-        </Container>
-      </Box>
+                </Flex>
+              </form>
+            </Box>
+          </Box>
+        </Flex>
+      </Flex>
     );
   };
 
   const Footer = () => {
     const router = useRouter();
     return (
-      <>
-        <Flex w={"6em"} m={["1rem 0rem", "3rem 1rem"]} position={"fixed"} alignItems={"center"} left={0} bottom={0} right={0} zIndex={"99"}>
-          <Button
-            variant={"none"}
-            onClick={() => {
-              router.back();
-            }}
-          >
-            <Center
-              w={["2.5rem", "2.5rem", "4rem", "4rem", "4rem"]}
-              h={["2.5rem", "2.5rem", "4rem", "4rem", "4rem"]}
-              mt={["0rem", "0rem", "0rem", "0rem", "0rem"]}
-              bgColor={"#F7B70C"}
-              border={["5px solid white", "5px solid white", "4px solid white", "4px solid white", "4px solid white"]}
-              borderRadius={"full"}
-              shadow={"0px 4px 4px rgba(0,0,0,0.25)"}
-            >
-              <Img src={"/expandLeft.svg"} w={["2rem", "2rem", "2rem", "2rem", "2rem"]} h={["1.2rem", "1.2rem", "2rem", "2rem", "2rem"]} />
-            </Center>
-          </Button>
-        </Flex>
-      </>
+      <Center position={["relative", "absolute"]} left={0} right={0} bottom={["", 0]} mb={["3vh", "5vh"]} mt={["8vh", "0"]}>
+        <Text color={"#1B4173"} fontSize={"sm"} fontWeight={"bold"}>
+          MAXIMA 2022
+        </Text>
+      </Center>
     );
   };
 
@@ -91,7 +160,13 @@ const login: NextPage = () => {
     <Layout>
       <Flex minH={"100vh"} bgColor={"white"}>
         <Box w={"full"} zIndex={"0"}>
-            
+          <Box mt={"9vh"}>
+            <MaximaLogo />
+          </Box>
+          <Center position={["relative", "absolute"]} left={0} right={0} top={0} bottom={0}>
+            <LoginForm />
+          </Center>
+          <Footer />
         </Box>
       </Flex>
     </Layout>
--- pages/register.tsx
+++ pages/register.tsx
@@ -39,65 +39,255 @@ import {
   InputLeftElement,
   Select,
   InputLeftAddon,
+  InputRightAddon,
 } from "@chakra-ui/react";
 
 const register: NextPage = () => {
   const MaximaLogo = () => {
     return (
-      <Center>
-        <Img src={"/Register/MaximaLogo.svg"} w={["9rem"]} />
+      <Center mt={["-3vh", "5vh"]} position={["relative", "absolute"]} left={0} right={0} top={0}>
+        <Img display={["none", "block"]} src={"/Register/MaximaLogo.svg"} w={["9rem"]} />
+        <Img display={["block", "none"]} src={"/Register/MaximaLogo2.svg"} w={["3rem"]} />
       </Center>
     );
   };
 
   const RegisterForm = () => {
     return (
-      <Flex padding={"1.5em 2.5em 2em 2.5em"} borderRadius={"lg "} boxShadow={"-1.2px 5px 4px 0px rgb(0,0,0,0.25)"} bgColor={"#fff"} justifyContent={"center"} alignItems={"center"} blur={15} border={"1px solid rgb(27, 65, 115, 0.25)"}>
-        <Box>
+      <Flex
+        w={["full", "auto"]}
+        h={["full", "auto"]}
+        // mt={"4vh"}
+        padding={["0 2em", "0.8em 2.5em 2em 2.5em"]}
+        borderRadius={["none", "lg"]}
+        boxShadow={["none", "-1.2px 5px 4px 0px rgb(0,0,0,0.25)"]}
+        bgColor={"#fff"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        blur={[0, 15]}
+        border={["none", "1px solid rgb(27, 65, 115, 0.25)"]}
+        overflowY={"auto"}
+        zIndex={1}
+      >
+        <Box w={"full"}>
           <Center>
-            <Text fontSize={"3xl"} fontWeight={"bold"} color={"#1B4173"}>
+            <Text fontSize={["3xl", "3xl", "3xl", "2xl", "3xl"]} fontWeight={"bold"} color={"#1B4173"}>
               Daftar Sekarang
             </Text>
           </Center>
-          <Center>
-            <Text fontSize={"md"} color={"#1B4173"} fontWeight={"medium"}>
+          <Center mb={["2em", "0"]}>
+            <Text fontSize={["md", "md", "md", "sm", "md"]} color={"#1B4173"} fontWeight={"medium"}>
               Sudah punya akun? <span style={{ color: "#F7B70C", fontWeight: "bold" }}>Masuk</span>
             </Text>
           </Center>
-          <Box mt={"1em"}>
+          <Box w={["full", "40em", "40em", "35em", "40.5em"]} mt={"1em"}>
             <form>
               <FormControl>
-                <Stack direction={"row"} spacing={4}>
-                  <Box w={"25em"}>
-                    <FormLabel textColor={"#1B4173"} fontWeight={"semibold"}>
-                      Nama Lengkap
-                    </FormLabel>
-                    <InputGroup>
-                      <InputLeftElement pointerEvents="none" />
-                      <Input borderColor={"#E2E8F0"} placeholder={""} type={"text"} name={"name"} textColor={"black"} border={"solid"} borderRadius={"full"} _hover={{ border: "solid #CBD5E0" }} />
-                    </InputGroup>
-                  </Box>
-                  <Box w={"18em"}>
-                    <FormLabel textColor={"#1B4173"} fontWeight={"semibold"}>
-                      NIM
-                    </FormLabel>
-                    <InputGroup>
-                      <InputLeftElement pointerEvents="none" />
-                      <InputLeftAddon children={"000000"} bgColor={"#F7B70C"} color={"white"} borderRadius={"full"} />
-                      <Input borderLeft={"none"} borderColor={"#E2E8F0"} placeholder={""} type={"text"} name={"name"} textColor={"black"} border={"solid"} borderRadius={"full"} _hover={{ border: "solid #CBD5E0" }} />
-                    </InputGroup>
-                  </Box>
+                <Stack direction={["column"]} spacing={[5, 4]}>
+                  <Stack direction={["column", "row"]} spacing={[5, 4]}>
+                    <Box w={["full", "32em", "32em", "32em", "32em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Nama Lengkap
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"Nama Lengkap"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={["full", "full"]}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box w={["full", "18em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        NIM
+                      </FormLabel>
+                      <InputGroup>
+                        <InputLeftAddon fontSize={"sm"} m={"auto"} p={2} children={"000000"} bgColor={"#F7B70C"} color={"white"} borderRadius={"full"} />
+                        <Input
+                          size={"md"}
+                          borderLeft={"none"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={""}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                  </Stack>
+                  <Stack direction={["column", "row"]} spacing={[5, 4]}>
+                    <Box w={["full", "17em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Tempat Lahir
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"Tempat lahir"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box w={["full", "full", "10.5em", "9.5em", "14em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Tanggal Lahir
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"DD/MM/YYYY"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"date"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box w={["full", "18em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Jenis Kelamin
+                      </FormLabel>
+                      <InputGroup>
+                        <Select size={"md"} name={"name"} textColor={"black"} border={"solid"} borderRadius={"full"} _hover={{ border: "solid #CBD5E0" }}>
+                          <option value="option1">Option 1</option>
+                          <option value="option2">Option 2</option>
+                          <option value="option3">Option 3</option>
+                        </Select>
+                      </InputGroup>
+                    </Box>
+                  </Stack>
+                  <Stack direction={["column", "row"]} spacing={[5, 4]}>
+                    <Box w={["full", "32em", "32em", "32em", "32em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Program Studi
+                      </FormLabel>
+                      <Select size={"md"} borderColor={"#E2E8F0"} placeholder={"Pilih prodi"} name={"name"} textColor={"black"} border={"solid"} borderRadius={"full"} _hover={{ border: "solid #CBD5E0" }}>
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                      </Select>
+                    </Box>
+                    <Box w={["full", "18em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Angkatan
+                      </FormLabel>
+                      <InputGroup>
+                        <Select size={"md"} borderColor={"#E2E8F0"} placeholder={"2021"} name={"name"} textColor={"black"} border={"solid"} borderRadius={"full"} _hover={{ border: "solid #CBD5E0" }}>
+                          <option value="option1">Option 1</option>
+                          <option value="option2">Option 2</option>
+                          <option value="option3">Option 3</option>
+                        </Select>
+                      </InputGroup>
+                    </Box>
+                  </Stack>
+                  <Stack direction={["column", "row"]} spacing={[5, 4]}>
+                    <Box w={["full"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Email Student
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderRight={"none"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"Email"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                        <InputRightAddon fontSize={"sm"} p={3} children={"@student.umn.ac.id"} bgColor={"#F7B70C"} color={"white"} borderRightRadius={"full"} />
+                      </InputGroup>
+                    </Box>
+                  </Stack>
+                  <Stack direction={["column", "row"]} spacing={[5, 4]}>
+                    <Box w={["full", "17em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Whatsapp
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"Nomor WhatsApp"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box w={["full", "14em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        ID LINE
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"ID LINE"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                    <Box w={["full", "18em"]}>
+                      <FormLabel display={["none", "block"]} fontSize={"sm"} textColor={"#1B4173"} fontWeight={"semibold"}>
+                        Instagram
+                      </FormLabel>
+                      <InputGroup>
+                        <Input
+                          size={"md"}
+                          borderColor={"#E2E8F0"}
+                          placeholder={"Username Instagram"}
+                          _placeholder={{ opacity: 1, color: "#CBD5E0" }}
+                          type={"text"}
+                          name={"name"}
+                          textColor={"black"}
+                          border={"solid"}
+                          borderRadius={"full"}
+                          _hover={{ border: "solid #CBD5E0" }}
+                        />
+                      </InputGroup>
+                    </Box>
+                  </Stack>
                 </Stack>
-                <FormLabel mt={"1em"} fontFamily="rubik" textColor={"black"}>
-                  Role
-                </FormLabel>
-                <Select borderColor={"#CBD5E0"} placeholder="Pilih Role" name="role" textColor={"black"} border={"solid"}>
-                  <option value="panitia">Panitia</option>
-                  <option value="organisator">Organisator</option>
-                </Select>
               </FormControl>
-              <Flex w={"100%"} justifyContent={"center"} py={3} mt={"0.5em"}>
-                <Button px={"2.1em"} borderRadius={"full"} type={"submit"} color={"#fff"} colorScheme={"orange"} bgColor={"#F7B70C"}>
+              <Flex w={"100%"} justifyContent={"center"} mt={"2em"}>
+                <Button w={["full", "auto"]} px={["2.1em"]} borderRadius={"full"} type={"submit"} color={"#fff"} colorScheme={"orange"} bgColor={"#F7B70C"}>
                   DAFTAR
                 </Button>
               </Flex>
@@ -111,7 +301,7 @@ const register: NextPage = () => {
   const Footer = () => {
     const router = useRouter();
     return (
-      <Center position={"absolute"} left={0} right={0} bottom={0} mb={"5vh"}>
+      <Center position={["relative", "absolute"]} left={0} right={0} bottom={["", 0]} mb={["3vh", "5vh"]} mt={["8vh", "0"]}>
         <Text color={"#1B4173"} fontSize={"sm"} fontWeight={"bold"}>
           MAXIMA 2022
         </Text>
@@ -121,12 +311,12 @@ const register: NextPage = () => {
 
   return (
     <Layout>
-      <Flex minH={"100vh"} bgColor={"white"} bgImage={["/Welcoming/HoMEWelcomingP.png", "/Register/register.jpg", "/Register/register.jpg", "/Register/register.jpg"]} bgPosition={"center"} bgSize={"contain"} bgRepeat={"no-repeat"}>
+      <Flex minH={"100vh"} bgColor={"white"} bgImage={["", "", "", "/Register/register.jpg"]} bgPosition={"center"} bgSize={"cover"} bgRepeat={"no-repeat"}>
         <Box w={"full"} zIndex={"0"}>
           <Box mt={"9vh"}>
             <MaximaLogo />
           </Box>
-          <Center position={"absolute"} left={0} right={0} top={0} bottom={0}>
+          <Center position={["relative", "absolute"]} left={0} right={0} top={0} bottom={0}>
             <RegisterForm />
           </Center>
           <Footer />
