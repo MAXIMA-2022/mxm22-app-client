48d338c3b478f74ec370198140c2d2441f248e5d
BANGATA <atanasiusditya@gmail.com>
2022-08-23 19:48:39 +0700

    fix: header logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children }: any) => {
     <Box>
       <Head>
         <title>MAXIMA 2022</title>
-        <link rel="shortcut icon" href="https://storage.googleapis.com/mxm22-bucket-test/maximaLogo.png" />
+        <link rel="shortcut icon" href="/maximaLogo.png" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,800;0,900;1,200;1,300&family=Rubik:wght@300;400&display=swap" rel="stylesheet" />
       </Head>
       <Box bgColor={"white"}>{children}</Box>
